872d36be74fccd7f8f54e8bec29c420afab4b6bd
IureLima <iurelimasilva@gmail.com>
2025-09-19 15:27:32 -0300

    feat: create the search component

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,9 +1,22 @@
+import { useRef } from 'react';
 import './SearchBar.css'
 
 function SearchBar(){
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleClickIcon = () => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  };
+
   return (
-    <div>
-      <p>searchbar</p>
+    <div className='search-bar'>
+      <span className="search-icon" onClick={handleClickIcon}></span>
+      <form action="">
+        <input type="text" placeholder='White city name' id='city' ref={inputRef} autoComplete='false'/>
+        <button>Search</button>
+      </form>
     </div>
   )
 }
