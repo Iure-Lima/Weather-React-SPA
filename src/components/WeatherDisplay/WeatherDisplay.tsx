8a0fbd98bf8063f060429df0e5df79569be0ce4b
IureLima <iurelimasilva@gmail.com>
2025-09-19 17:19:36 -0300

    feat: add main structure for the Weather display

---
+++ src/components/WeatherDisplay/WeatherDisplay.tsx
@@ -0,0 +1,13 @@
+import type { WeatherCardModel } from "../../models/weather.car.model";
+import "./WeatherDisplay.css"
+
+function WeatherDisplay(weatherData: WeatherCardModel){
+  console.log(weatherData)
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default WeatherDisplay;
