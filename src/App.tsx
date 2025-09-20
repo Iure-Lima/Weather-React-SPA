cd90fc3bb43848e441d2486c997bcfd10d66e54a
IureLima <iurelimasilva@gmail.com>
2025-09-19 21:54:57 -0300

    refactore: modify the color for seach button hover in dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import './App.css'
 import ThemeSwitcher from './components/ThemeSwitcher/ThemeSwitcher.tsx'
 import SearchBar from './components/SearchBar/SearchBar.tsx';
+import WeatherDisplay from './components/WeatherDisplay/WeatherDisplay.tsx';
+import type { WeatherCardModel } from './models/weather.car.model.ts';
 
 function App() {
   const [themeApp, setThemeApp] = useState("light");
@@ -9,6 +11,32 @@ function App() {
   const handleSwitcherThemeApp = () => {
     setThemeApp(themeApp === "light" ? "dark" : "light")
   }
+
+  const mockData: WeatherCardModel =  {
+  location: {
+    name: "Paris",
+    region: "Ile-de-France",
+    country: "France",
+    time_zone: "Europe/Paris",
+    local_time: "2025-09-19 19:19"
+  },
+  weather_information: {
+    last_updated: "2025-09-19 19:15",
+    temp_celsius: 30.1,
+    temp_fahrenheit: 86.2,
+    feelslike_celsius: 27.9,
+    feelslike_fahrenheit: 82.2,
+    is_day: 1,
+    condition: "Sunny",
+    condition_icon: "//cdn.weatherapi.com/weather/64x64/day/113.png",
+    wind_mph: 4.3,
+    wind_kph: 6.8,
+    wind_dir: "S",
+    humidity: 33,
+    cloud: 0,
+    uv: 0.2
+  }
+};
 
   return (
     <main className={`${themeApp}`}>
@@ -18,6 +46,7 @@ function App() {
       </header>
       <section className='container'>
         <SearchBar />
+        <WeatherDisplay weatherData={mockData}/>
       </section>
     </main>
   )
--- src/components/WeatherDisplay/WeatherDisplay.tsx
+++ src/components/WeatherDisplay/WeatherDisplay.tsx
@@ -1,12 +1,76 @@
+import { useState } from "react";
 import type { WeatherCardModel } from "../../models/weather.car.model";
-import "./WeatherDisplay.css"
+import './WeatherDisplay.css'
 
-function WeatherDisplay(weatherData: WeatherCardModel){
-  console.log(weatherData)
+function WeatherDisplay(props: {weatherData: WeatherCardModel | null} ){
+  const [tempMetric, setTempMetric] = useState("cel")
+  const [windMetric, setWindMetric] = useState("kph")
+
+  const handleTempMetric = (temp:string) => {
+    setTempMetric(temp)
+  }
+
+  const handleWindMetric = (wind:string) => {
+    setWindMetric(wind)
+  }
+
+
   return (
-    <div>
+    <section className="weather-display">
+      <article className="weather-card">
+        <div className="card-header">
+          <div>
+            <h2>
+            {props.weatherData?.location.name}
+            </h2>
+            <p>{props.weatherData?.location.region}</p>
+          </div>
+          <div>
+            <p>{props.weatherData?.location.time_zone}</p>
+            <p>{props.weatherData?.location.local_time}</p>
+          </div>
+        </div>
 
-    </div>
+        <div className="card-body">
+          <img src={props.weatherData?.weather_information.condition_icon} alt="weather icon" />
+          <div>
+            <h3>{tempMetric === "cel" ? `${props.weatherData?.weather_information.temp_celsius} °C` : `${props.weatherData?.weather_information.temp_fahrenheit} °F`}</h3>
+            <p><strong>{props.weatherData?.weather_information.condition}</strong></p>
+          </div>
+        </div>
+
+        <div className="card-information">
+          <div className="information-data">
+            <p>Feels Like {tempMetric === "cel" ? `${props.weatherData?.weather_information.feelslike_celsius} °C` : `${props.weatherData?.weather_information.feelslike_fahrenheit} °F`}</p>
+          </div>
+          <div className="information-data">
+            <div className="btn-container">
+              <button className={tempMetric === "cel" ? "activate" : ""} onClick={() => handleTempMetric("cel")}>C°</button>
+              <button className={tempMetric === "fah" ? "activate" : ""} onClick={() => handleTempMetric("fah")}>F°</button>
+            </div>
+          </div>
+          <div className="information-data">
+            <p>Wind {windMetric === "kph" ? `${props.weatherData?.weather_information.wind_kph} KP/H` : `${props.weatherData?.weather_information.wind_mph} MP/H`}</p>
+          </div>
+          <div className="information-data">
+            <div className="btn-container">
+              <button className={windMetric === "mph" ? "activate" : ""} onClick={() => handleWindMetric("mph")}>mph</button>
+              <button className={windMetric === "kph" ? "activate" : ""} onClick={() => handleWindMetric("kph")}>kpm</button>
+            </div>
+          </div>
+          <div className="information-data column">
+            <p>Humidity {props.weatherData?.weather_information.humidity}%</p>
+          </div>
+          <div className="information-data">
+            <p>Cloud {props.weatherData?.weather_information.cloud}%</p>
+          </div>
+          <div className="information-data">
+            <small>Last Updated: {props.weatherData?.weather_information.last_updated}</small>
+          </div>
+        </div>
+
+      </article>
+    </section>
   )
 }
 
