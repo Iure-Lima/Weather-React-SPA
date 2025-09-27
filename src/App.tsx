0c8ecb1e3be817b6c7f17f66d82e735493ff9fdd
IureLima <iurelimasilva@gmail.com>
2025-09-27 18:18:15 -0300

    feat: add lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-
-
-import Home from "./components/Home/Home.tsx";
-import WeatherDetails from "./components/WeatherDetails/WeatherDetails.tsx";
-import About from "./components/About/About.tsx";
+import { lazy } from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
+import Home from "./components/Home/Home.tsx";
+const WeatherDetails = lazy(() => import("./components/WeatherDetails/WeatherDetails.tsx"))
+const About = lazy(() => import("./components/About/About.tsx"))
 import AppLayout from "./components/AppLayout/AppLayout.tsx";
 
 function App() {
